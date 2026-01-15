bbaaf616a8c1657e16b665d062641dc74c2c93b3
Isha <izade83@gmail.com>
2026-01-15 18:03:55 -0500

    Fix ESLint issues for Netlify build

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,4 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
-
 export const Footer = () => {
   return (
     <footer className="footer">
@@ -12,7 +7,10 @@ export const Footer = () => {
           <Col size={12} sm={6}>IZ</Col>
           <Col size={20} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
+              <button type="button" className="your-link-styles">
+  ...
+</button>
+
               
             
             </div>
